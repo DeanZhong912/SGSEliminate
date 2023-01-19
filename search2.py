41e4d18fbba7df535d2f738505ceb7c4de92ced9
DeanZhong912 <50126489+DeanZhong912@users.noreply.github.com>
2023-01-19 12:55:52 +0800

    Add search2.py
    求解题目的模糊解

---
+++ search2.py
@@ -0,0 +1,76 @@
+import re
+from collections import Counter
+from heapq import nlargest
+# 两种算法
+# 过筛法，去掉问题中返回结果较多的字，留下
+# TopK 找到台词集中包含题目字符最多个数的台词
+
+# 台词文本路径
+DeadPath = './dead.txt'
+SkillPath1 = './skill1.txt'
+SkillPath2 = './skill2.txt'
+VictoryPath = './victory.txt'
+
+# 过筛法
+frequentWords = {'为','的','以','之','我','你','他','而','一','又','了','不','也','其','已','如','在','此','无','有','得','何','可'}
+# 不过筛
+#frequentWords = {''}
+
+
+# 获取文本下的台词
+def getTaiciDB(path):
+    taiciList = []
+    file = open(path,"r")
+    lines = file.readlines()
+    for line in lines:
+        line = line.strip()
+        taiciList.append(line)
+    return taiciList
+
+# 从字符串集合中找出包含特定字符集最多的字符个数的前K个字符串
+def find_top_k(strings, charset, k):
+    count = Counter()
+    for string in strings:
+        count[string] = sum(1 for c in string if c in charset)
+    return nlargest(k, count, key=count.get)
+
+# 搜索台词
+def searchTaiCi(Question,TaiciDB):
+    # 返回前十个
+    result = find_top_k(TaiciDB,Question,2)
+    return result
+
+# 遍历台词库搜索
+def run(Question,path):
+    TaiciDB = getTaiciDB(path)
+    result = searchTaiCi(Question,TaiciDB)
+    print(path+"----\nResult:")
+    if result != []:
+        print(result)
+    else:
+        print("No Matching in the "+Path)
+    return result
+
+# 读取问题
+f = open("./res.txt", "r")
+input = f.readline()
+Question = list(input)
+del Question[-1]
+print("Question:")
+#print(Question)
+Qs = set(Question)
+# 去掉Question中 频繁出现的字（查询搜索结果多的）
+QuestionP = list(Qs-frequentWords)
+#print(Qs)
+#print(QuestionP)
+
+# 运行搜索
+V = run(QuestionP,VictoryPath)
+D = run(QuestionP,DeadPath)
+S1 = run(QuestionP,SkillPath1)
+S2 = run(QuestionP,SkillPath2)
+
+print("-----ALL RESULT-----")
+for bx in set(V+D+S1+S2):
+    print(bx)
+
